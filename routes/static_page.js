f8783d0047ed25c347ad3ca372d54a1af61bf97f
Mingchen Zhang <zmc9499@gmail.com>
2017-02-20 14:33:14 -0500

    initial commit

---
+++ routes/static_page.js
@@ -0,0 +1,16 @@
+var Express = require('express');
+var BodyParser = require('body-parser');
+var When = require('when');
+
+exports.getRoute = function (s) {
+    var router = Express.Router();
+
+    var urlParser = BodyParser.urlencoded({extended: false, limit: '10kb'});
+    var jsonParser = BodyParser.json({limit: '10kb'});
+
+    router.get('/', function (req, res, next) {
+        res.render('home-page');
+    });
+
+    return router;
+};
